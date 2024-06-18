c3c27c5d154b70e5e89e0aa8f351c97a703ea233
Pravesh Grewal <gurahpravesh@gmail.com>
2024-06-18 12:27:40 +0530

    18-06-2024

---
+++ Practice/Section.2.Python_Programming_language/2.Python_Programming_Language/python_basic.py
@@ -0,0 +1,33 @@
+#case sensitive 
+Name = "Pravesh"
+name = "Grewal"
+print(Name)
+print(name)
+
+#single line comment (this is single line comment)
+"""
+multi line comment (this is multi line comment)
+"""
+
+#indentation
+age = 69
+if age > 30:
+    print(age)
+
+print(age)
+
+# line continuation
+sum = 3+9+9+7+86\
+    +8+96
+
+print(sum)
+
+#multiple statements in single line
+a = 5; b = 6; c = 7
+print(a+b+c)
+
+#variable assignment
+age = 22
+naam = "jiger"
+print(type(age))
+print(type(naam))
